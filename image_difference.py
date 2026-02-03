5e319c055eee71fd1269f57408fab44a3a83bb40
salihakay <135813951+salihky1@users.noreply.github.com>
2026-02-03 21:12:21 +0300

    Fix typo in variable name for image difference

--- image_difference.py
+++ image_difference.py
@@ -2,9 +2,9 @@ import cv2
 img1=cv2.imread("")
 img2=cv2.imread("")
 
-fark=cv2.substract(img1,img2)
+different=cv2.substract(img1,img2)
 
-cv2.imshow("1",fark)
+cv2.imshow("1",different)
 
 
 cv2.waitKey(0)
